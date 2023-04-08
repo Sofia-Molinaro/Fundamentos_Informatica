d29f48ab5911481de95fa18864de7f29d248791d
Sofia-Molinaro <molinarosofia1@gmail.com>
2023-04-08 11:16:58 -0300

    Actualización Guía 2

---
+++ Guía_2/Ejercicio_27.py
@@ -0,0 +1,48 @@
+# Ejecute el siguiente código y diga qué hace y qué elementos de programación se utilizan: variables,
+# valores, operadores y sentencias.
+
+# a-
+# print('hola')
+# Qué hace: Imprime el str "hola".
+# Qué elementos de programación se utilizan: utiliza una sentencia print para imprimir y mostrar por pantalla "hola" de tipo str.
+
+# b-
+# print (2)
+# Qué hace: Imprime el número 2.
+# Qué elementos de programación se utilizan: utiliza una sentencia print para imprimir y mostrar por pantalla el número.
+
+# c-
+# nombre=input('Ingrese un nombre')
+# print (nombre)
+# Qué hace: Este programa le pide al usuario que ingrese un nombre que es de tipo str; luego lo imprime por pantalla.
+# Qué elementos de programación se utilizan: utiliza un input para perdirle el nombre al usuario y una sentencia print para imprimir y mostrar por pantalla el nombre de tipo str.
+
+# d-
+# edad=input('Ingrese la edad')
+# print (edad)
+# Qué hace: imprime la edad que el usuario ingresó por pantalla. 
+# Qué elementos de programación se utilizan: utiliza un input para pedirle al usuario que ingrese su edad, además, una sentencia print para imprimir y mostrar por pantalla la edad de tipo str.
+
+# e-
+# print (2*3)
+# Qué hace: imprime el producto de la multiplicación entre 2 y 3. 
+# Qué elementos de programación se utilizan: utiliza una sentencia print para imprimir y mostrar por pantalla el resultado de la multiplicación.
+
+# f-
+# print (4%2)
+# Qué hace: imprime y muestra por pantalla el resto de la división de 4 dividido 2. 
+# Qué elementos de programación se utilizan: utiliza un print para imprimir y mostrar por pantalla el producto de la multiplicación.
+
+# g-
+# num1=input('Ingrese un número')
+# num2=input('Ingrese otro número')
+# print (num1+num2)
+# Qué hace: el programa le pide al usuario dos números que son tomados por el compilador de tipo str. Luego imprime y muestra por pantalla la concatenación de los valores que el usuario le asigna a las variables num1 y num2 de tipo str. 
+# Qué elementos de programación se utilizan: utiliza un input en cada caso para pedirle al usuario que ingrese un número y luego otro número pero estos los toma como str porque no el programa no casteo las variables num1 y num2 para que tome los valores ingresados por el usuario como un número. Luego utiliza un print para mostrar la concatenación de las variables num1 y num2
+
+# h-
+# num1 = int(input('Ingrese un número'))
+# num2 = int(input('Ingrese otro número'))
+# print (num1+num2)
+# Qué hace: el programa le pide al usuario dos números de tipo int. Luego imprime y muestra por pantalla la suma de los valores que el usuario le asigna a las variables num1 y num2 de tipo int.
+# Qué elementos de programación se utilizan: utiliza un input en cada variable para pedirle al usuario que ingrese un número de tipo int. Luego utiliza un print para imprimir y mostrar por pantalla la suma de los valores que el usuario ingreso en las variables num1 y num2 de tipo int. 
