db5fd62689fb59e9dbf3fa49a3f680ab115f392f
Sofia-Molinaro <molinarosofia1@gmail.com>
2023-04-08 09:36:27 -0300

    Actualización Guía 2

---
+++ Guía_2/Ejercicio_22.py
@@ -0,0 +1,7 @@
+# lista=[1,True,['a','b','c'], "hola"]
+# lista[2]
+# print (lista)
+
+lista=[1,True,['a','b','c'], "hola"]
+lista = lista[2]
+print (lista)
---
+++ Guía_2/Ejercicio_23.py
@@ -0,0 +1,10 @@
+# Dada la siguiente información, elija una estructura de datos que permita guardarla
+# adecuadamente
+# ● Guerra del Peloponeso 431 a.C
+# ● Revolución de Mayo 1810 d.C
+# ● Llegada de los españoles a América 1492 d.C
+# ● Comienzo de la construcción de la gran Muralla China 214 a.C
+
+tupla = ("● Guerra del Peloponeso 431 a.C, \n● Comienzo de la construcción de la gran Muralla China 214 a.C, \n● Llegada de los españoles a América 1492 d.C, \n● Revolución de Mayo 1810 d.C")
+
+print(tupla)
