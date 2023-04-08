62366eb49e210f47ec63ffaf2b71a1ea61bfc229
Sofia-Molinaro <molinarosofia1@gmail.com>
2023-04-08 15:21:46 -0300

    Actualización Guía 2

---
+++ Guía_2/Ejercicio_28.py
@@ -0,0 +1,31 @@
+# #Asígnele a las variables num1 y num2 los distintos valores indicados en la tabla.
+# Realice las siguientes cuentas con las operaciones indicadas almacenando el resultado en la variable
+# resultado.
+# En la tabla que aparece a continuación coloque los resultados obtenidos y justifíquelo.
+
+#     Valor de num1              Valor de num2           Operación          Valor de resultado           Justificación
+
+# 1)       5                          2                    //
+
+# 2)      7.2                       9.8                    *
+
+# 3)        7                       3.1                    -
+
+# 4)     10.45                       7                     +
+
+# 5)   "republica"                   3                     *
+
+# 1) RESULTADO: el resultado es 2
+#    JUSTIFICACIÓN: porque la operación me indica una división entera entre los valores de num1 y num2.
+
+# 2) RESULTADO: el resultado es 70.56
+#    JUSTIFICACIÓN: porque la operación me indica que multiplique el valor de num1 y el valor de num2.
+
+# 3) RESULTADO: el resultado es 3.9
+#    JUSTIFICACIÓN: porque la operación me indica que reste el valor num1 con el valor num2.
+
+# 4) RESULTADO: el resultado es 17.45
+#    JUSTIFICACIÓN: porque la operación me indica que sume el valor de num1 y el valor num2.
+
+# 5) RESULTADO: el resultado es "republica republica republica"
+#    JUSTIFICACIÓN: porque la operación me indica una multiplicación de un str, por lo tanto se repetirá, en este caso, tres veces el valor asignado a la variable num1. 
