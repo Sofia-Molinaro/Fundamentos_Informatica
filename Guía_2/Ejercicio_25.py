59624f6775f1a3c20656cbefd66ed2cb93cbe410
Sofia-Molinaro <molinarosofia1@gmail.com>
2023-04-08 09:58:03 -0300

    Actualización Guía 2

--- Guía_2/Ejercicio_25.py
+++ Guía_2/Ejercicio_25.py
@@ -3,9 +3,9 @@
 # En pantalla debe aparecer “ Su nombre es xxx y su edad es xx”
 # ¿Hizo uso de variables? ¿Cuáles? Sí. utilicé las variables nombre y edad. 
 # ¿Hizo uso de valores? ¿Cuáles? Los valores de nombre y edad serán ingresados por el usuario. 
-# ¿Hizo uso de operadores? ¿Cuáles?  Utilicé el operador suma (+) para concatenar los strings.
+# ¿Hizo uso de operadores? ¿Cuáles?  Utilicé el operador suma (+) para concatenar strings.
 # ¿Qué sentencias utilizó? Utilicé input y print
 
 nombre = input("Ingrese su nombre: ")
 edad = int(input("Ingrese su edad: "))
-print('Su nombre es' + nombre + 'y su edad es' + edad)
+print("Su nombre es " + nombre + "y su edad es ", edad, "años.")
