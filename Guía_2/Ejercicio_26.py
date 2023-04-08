e8a9de691943e32538ed1bc23e8b155945285e43
Sofia-Molinaro <molinarosofia1@gmail.com>
2023-04-08 10:32:19 -0300

    Actualización Guía 2

---
+++ Guía_2/Ejercicio_26.py
@@ -0,0 +1,33 @@
+# Ejecute el siguiente código e indique, en cada caso, con qué valor finaliza la variable x. Justifique su respuesta.
+# a)
+#x = 10
+#x = x**2
+#print (x)
+# En este caso x = 100, porque en la primera línea toma el valor de 10. Sin embargo, en la segunda línea ese valor cambia porque x, ahora, es x que es igual a 10 elevada al cuadrado que es 100. Por lo cual la variable x cambia su valor a 100.  
+
+# b)
+#x=30
+#x= x % 4
+#print (x)
+# En este caso x parte siendo igual a 30. Sin embargo, en la segunda línea de código el valor de x se modifica. x que era igual a 30 es dividido por 4 y da el resto 2. Ahora x es 2. 
+
+# c)
+#a= "4"
+#b= "3"
+#x= a + b
+#print (x)
+# En este caso x es igual a "43". El operador suma actua como concatenador de dos strings asignados a la variable a = "4" y b = "3". Me doy cuenta que son strings porque los valores están escritos entre comillas. 
+
+# d)
+# a = "4"
+# b = 3
+# x = a * b
+# print (x)
+#En este caso x será igual a tres veces 4 , es decir, 444. Debido a que se está multiplicando 3 veces el string "4" de la variable a.
+
+# e)
+# a = 4
+# b = 3
+# x = "a" * b
+# print (x)
+#En este caso x será igual a "aaa" debido a que se multiplica el string "a" por el valor asignado a la variable b que es 3. Como el valor de b es 3 se repite esa cantidad de veces "a". 
