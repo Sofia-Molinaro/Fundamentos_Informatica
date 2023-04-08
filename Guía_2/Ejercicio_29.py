feaab373cdc0e479325cff45089055f9a57ca42e
Sofia-Molinaro <molinarosofia1@gmail.com>
2023-04-08 16:20:10 -0300

    Actualización Guía 2

---
+++ Guía_2/Ejercicio_29.py
@@ -0,0 +1,8 @@
+# Si se tiene las variables n1 = 124.25 y n2 = "33.40". Realizar las conversiones
+# necesarias para saber la división entera entre ellos y el resto.
+
+n1 = 124.25 
+n2 = 33.40
+divisionEntera = n1 // n2
+restoDeLaDivision = n1 % n2
+print(f"El resultado de la división entera de {n1} y {n2} es: {divisionEntera}. \nEl resto de la división de {n1} dividido {n2} es: {restoDeLaDivision}")
---
+++ Guía_2/Ejercicio_30.py
@@ -0,0 +1,16 @@
+#Pedir 5 palabras y mostrar la cantidad de letras que tienen en total.
+
+primerPalabra = input("Ingrese la primera palabra: ")
+print(f"La {len(primerPalabra)}") 
+
+segundaPalabra = input("Ingrese la segunda palabra: ")
+print(f"La {len(segundaPalabra)}") 
+
+terceraPalabra = input("Ingrese la tercera palabra: ")
+print(f"La {len(terceraPalabra)}") 
+
+cuartaPalabra = input("Ingrese la cuarta palabra: ")
+print(f"La {len(cuartaPalabra)}") 
+
+quintaPalabra = input("Ingrese la quinta palabra: ")
+print(f"La {len(quintaPalabra)}") 
