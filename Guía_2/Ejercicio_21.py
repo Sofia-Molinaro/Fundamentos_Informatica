b52bce03837896d097f82a2eff33cc11f889bf14
Sofia-Molinaro <molinarosofia1@gmail.com>
2023-04-08 09:13:26 -0300

    Actualización Guía 2

---
+++ Guía_2/Ejercicio_21.py
@@ -0,0 +1,18 @@
+# Dada la siguiente estructura: lista=[1,True,['a','b','c'], "hola"]. Ejecutar la siguientes
+# sentencias y describir los resultados.
+# a) print (lista[2])
+# b) print (lista [4])
+# c) lista.append (False)
+# print (lista)
+
+lista=[1,True,['a','b','c'], "hola"]
+# A)
+print (lista[2])
+
+# B) Dará un error porque no hay un componente 4 en la lista.
+#print (lista [4])
+print("\n")
+
+# C)
+lista.append (False)
+print (lista)
