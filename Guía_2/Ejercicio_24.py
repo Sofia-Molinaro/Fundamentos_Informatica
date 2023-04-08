383108a3249ef8ed75a6331f97eb006a66249f03
Sofia-Molinaro <molinarosofia1@gmail.com>
2023-04-08 09:46:24 -0300

    Actualización Guía 2

---
+++ Guía_2/Ejercicio_24.py
@@ -0,0 +1,10 @@
+# Escriba un programa que reciba un nombre ingresado por el usuario e imprima en la
+# pantalla un saludo.
+# En pantalla debe aparecer “¡Hola xxx!”
+# ¿Hizo uso de variables? ¿Cuáles? Sí. Uso la variable nombre para que el usuario ingrese un string. 
+# ¿Hizo uso de valores? ¿Cuáles? No use ninguno.
+# ¿Hizo uso de operadores? ¿Cuáles? No aunque podría haber usado el operador suma para concatenar los strings en el print
+# ¿Qué sentencias utilizó? Utilicé input y print
+
+nombre = input("Ingrese un nombre: ")
+print(f"¡Hola {nombre}!")
