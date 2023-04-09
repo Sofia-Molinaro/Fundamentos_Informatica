0c2bb9ffbcf4ed23739b70f734dcb68a3ba636b2
Sofia-Molinaro <molinarosofia1@gmail.com>
2023-04-08 21:03:03 -0300

    Actualización Guía 2

---
+++ Guía_2/Ejercicio_32.py
@@ -0,0 +1,5 @@
+#Mostrar el código ASCII de los caracteres “A”, “a” y “0”
+#la función ord, recibe un carácter y regresa un entero que representa el número unicode que representa a ese carácter.
+print(ord("A"))
+print(ord("a"))
+print(ord("0"))
---
+++ Guía_2/Ejercicio_33.py
@@ -0,0 +1,1 @@
+#Pedir la cuenta de mail al usuario y mostrar por separado su usuario y su dominio.
