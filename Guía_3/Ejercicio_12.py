c37b4f8eb2b7dc3af06373311accf29f7af5fbbd
Sofia-Molinaro <molinarosofia1@gmail.com>
2023-04-16 15:57:25 -0300

    Actualización Guía 3

---
+++ Guía_3/Ejercicio_12.py
@@ -0,0 +1,2 @@
+# Definir una función llamada calculo_nuevo_precio que reciba dos números, uno con el
+# precio anterior y otro con el número de porcentaje a aumentar y devuelva el precio aumentado.
