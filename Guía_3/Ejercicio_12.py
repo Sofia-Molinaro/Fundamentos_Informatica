a479548a76c818346d5fa651457b2de8e5059fb9
Sofia-Molinaro <molinarosofia1@gmail.com>
2023-04-16 19:57:54 -0300

    Actualización Guía 3

--- Guía_3/Ejercicio_12.py
+++ Guía_3/Ejercicio_12.py
@@ -1,2 +1,12 @@
 # Definir una función llamada calculo_nuevo_precio que reciba dos números, uno con el
 # precio anterior y otro con el número de porcentaje a aumentar y devuelva el precio aumentado.
+
+def calculo_nuevo_precio(precioAnterior, porcentajeAumentar):
+    precioAumentado =  ((porcentajeAumentar * precioAnterior) / 100) + precioAnterior
+    print(f"El precio aumentado es: ${precioAumentado}")
+    return precioAumentado 
+
+precio_anterior = float(input("Ingrese el precio anterior: $"))
+porcentaje_a_aumentar = float(input("Ingrese el porcentaje a aumentar: "))
+
+calculo_nuevo_precio(precio_anterior, porcentaje_a_aumentar)
